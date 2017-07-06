e944954d4777516cf1ae2a77c05c166292f1501f
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-05 20:57:45 -0500

    added some examples of arrays

---
+++ arrays_hashes/arrays.rb
@@ -0,0 +1,33 @@
+names = Array.new
+names[0] = "Ana"
+names[1] = "Maria"
+puts names
+
+#********
+names = %w{ Ana Maria Isabel }
+puts names
+
+#*********
+names = Array.new(2, "Ana")
+puts "#{names}"
+
+#*********
+nums = Array.[](1, 2, 3, 4)
+puts "#{nums}"
+nums = Array[1, 2, 3, 4]
+puts "#{nums}"
+
+#*********
+digits = Array(2..9)
+puts "#{digits}"
+num = digits.at(2)
+puts num
+
+#*********
+numbers = [4, 1, 51, 0, 78]
+puts "Total: #{numbers.length}"
+puts "#{numbers.sort}"
+puts "#{numbers.sort.first}"
+puts "#{numbers.sort.last}"
+puts numbers.reduce(:+)
+puts [1, 2, 3].reduce(:*)
